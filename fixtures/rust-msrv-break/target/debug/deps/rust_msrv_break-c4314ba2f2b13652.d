b5780df76211e0b3c53b9fe5c16037b2319c6b16
TomorrowCI Builder <tomorrowci@local.dev>
2026-08-05 17:10:15 +0800

    Stop tracking fixture cargo target/ artifacts

--- fixtures/rust-msrv-break/target/debug/deps/rust_msrv_break-c4314ba2f2b13652.d
+++
@@ -1,7 +0,0 @@
-C:\Users\1\Desktop\tomorrowci\fixtures\rust-msrv-break\target\debug\deps\rust_msrv_break-c4314ba2f2b13652.d: src\lib.rs
-
-C:\Users\1\Desktop\tomorrowci\fixtures\rust-msrv-break\target\debug\deps\librust_msrv_break-c4314ba2f2b13652.rlib: src\lib.rs
-
-C:\Users\1\Desktop\tomorrowci\fixtures\rust-msrv-break\target\debug\deps\librust_msrv_break-c4314ba2f2b13652.rmeta: src\lib.rs
-
-src\lib.rs:
--- fixtures/rust-msrv-break/target/debug/deps/toolchain_gate-62703b4e655fe8a7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\1\Desktop\tomorrowci\fixtures\rust-msrv-break\target\debug\deps\toolchain_gate-62703b4e655fe8a7.d: tests\toolchain_gate.rs
-
-C:\Users\1\Desktop\tomorrowci\fixtures\rust-msrv-break\target\debug\deps\toolchain_gate-62703b4e655fe8a7.exe: tests\toolchain_gate.rs
-
-tests\toolchain_gate.rs:
